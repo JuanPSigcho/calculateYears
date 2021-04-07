d951bb2a0fef7e4a2af6560a1b6270581b5364cc
JuanPSigcho <juanpsigcho@gmail.com>
2021-04-06 23:15:22 -0500

    Initial logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'calculateFree';
+  timeCalculated = 0;
+  yearsBase = 0;
+  monthBase = 0;
+  percentTime = 60;
+
+  calculateTime() {
+    //let calculateTime = 0;
+    if (this.yearsBase > 0 || this.monthBase > 0) {
+      console.log('years', this.yearsBase, this.monthBase);
+      this.timeCalculated = (this.yearsBase * this.percentTime) / 100;
+    }
+    console.log('entre', this.timeCalculated);
+  }
 }
